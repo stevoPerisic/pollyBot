c6479b66886e64b929c9a75b60e503d7a2b37e3b
Stevo Perisic <stevo.perisic@maestralsolutions.com>
2016-10-10 23:47:26 -0400

    this just for the quick test before we set up the botkit framework

--- bot.js
+++ bot.js
@@ -29,6 +29,25 @@ app.get('/webhook', function (req, res) {
 	}
 });
 
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
 // handler receiving messages
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
