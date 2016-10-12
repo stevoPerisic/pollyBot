11d656b970ab5d208480f8f8f0f6a288c0e344c0
Stevo Perisic <stevo.perisic@maestralsolutions.com>
2016-10-11 23:22:42 -0400

    fixed fb bot handler

--- bot.js
+++ bot.js
@@ -30,33 +30,25 @@ app.get('/webhook', function (req, res) {
 });
 
 // generic function sending messages
-function sendMessage(recipientId, message) {
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.FB_PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-};
-
-// This is the Botkit controller for facebook messenger
-var controllerFB = Botkit.facebookbot({
+var handler = require('./lib/fb_handler');
+handler.controllerFB = Botkit.facebookbot({
     debug: true,
     access_token: process.env.FB_PAGE_ACCESS_TOKEN,
     verify_token: process.env.FB_VERIFY_TOKEN
 });
 
-// new instance of the bot
-var bot = controllerFB.spawn({});
+var bot = handler.controllerFB.spawn({});
+
+// add our custom bot message handler because we are using express as our server for both the bot and the consumer/backend app
+controllerFB.handlerFB = require('./lib/fb_handler');
+
+// handler receiving messages
+app.post('/webhook', jsonParser, function (req, res) {
+	// console.log("FB request: \n");
+	// console.log(req);
+	handler.FBhandler(req.body, bot)
+    res.sendStatus(200);
+});
 
 // subscribe to page events
 request.post('https://graph.facebook.com/me/subscribed_apps?access_token=' + process.env.FB_PAGE_ACCESS_TOKEN,
@@ -74,19 +66,8 @@ request.post('https://graph.facebook.com/me/subscribed_apps?access_token=' + pro
   }
 );
 
-// add our custom bot message handler because we are using express as our server for both the bot and the consumer/backend app
-controllerFB.handlerFB = require('./lib/fb_handler');
-
-// handler receiving messages
-app.post('/webhook', jsonParser, function (req, res) {
-	// console.log("FB request: \n");
-	// console.log(req);
-	controllerFB.FBhandler(req.body, bot);
-    res.sendStatus(200);
-});
-
 // listen to some text
-controllerFB.hears(['Hello'], 'message_received', function (bot, message) {
+handler.controllerFB.hears(['Hello'], 'message_received', function (bot, message) {
   	bot.reply(message, {
     	"text":"Hello and welcome to Perisic Designs!" 
 	});
