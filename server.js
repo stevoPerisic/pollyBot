60e7ba60ed57d344ca74bd5d06c7433976940811
Stevo Perisic <stevo.perisic@maestralsolutions.com>
2016-10-17 23:00:14 -0400

    starting the bot after server starts, updated the package start script

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var router = require('./lib/router.js');
 var viewRender = require('./views/index.js');
+var bot = require('./bot.js');
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -10,4 +11,5 @@ router(app, viewRender);
 
 app.listen((process.env.PORT || 5000), function () {
   console.log('Listening on port 5000');
+  bot();
 });
